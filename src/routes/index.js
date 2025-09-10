2ae482a45994e53c17efac3305f1053e17c62bd7
blaze-of-fire <carnold@byupathway.edu>
2025-09-10 13:22:29 -0600

    new changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,15 @@
-const router = require("express").Router();
+const routes = require('express').Router();
+const temple = require('./temple');
 
-router.get("/", (req, res) => { res.send("Hello World!"); });
+routes.use('/temples', temple);
+routes.use(
+  '/',
+  (docData = (req, res) => {
+    let docData = {
+      documentationURL: 'https://nathanbirch.github.io/nathan-byui-api-docs',
+    };
+    res.send(docData);
+  })
+);
 
-router.use("/contacts", require("./contacts"));
-
-module.exports = router;
+module.exports = routes;
