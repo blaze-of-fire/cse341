fc7fe318384cfbabe29f838eb019c4d9427df317
blaze-of-fire <carnold@byupathway.edu>
2025-09-13 11:17:11 -0600

    Update contacts.js route

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -3,8 +3,16 @@ const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
 
+
 router.get("/", contactsController.getAllContacts);
 
 router.get("/:id", contactsController.getContactById);
+
+router.post("/", contactsController.createContact):
+
+router.put("/:id", contactsController.updateContact);
+
+router.delete("/:id",contactsController.deleteContact);
+
 
 module.exports = router;
