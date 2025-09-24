57229a2635a35fa7e63a536d0701cf02893557d9
blaze-of-fire <carnold@byupathway.edu>
2025-09-24 13:20:07 -0600

    test

--- server.js
+++ server.js
@@ -58,9 +58,9 @@ passport.deserializeUser((user, done) => {
     done(null, user);
 });
 
-app.get((req, res) => { res.send(req.session.user) !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out"})
+app.get("/", (req, res) => { res.send(req.session.user) !== undefined ? `Logged in as ${req.session.user.displayName}` : "Logged Out"})
 
-app.get("github/callback", passport.authenticate("github", {
+app.get("/github/callback", passport.authenticate("github", {
     failureRedirect: "/api-docs", session: false}),
     (req, res) => {
         req.session.user = req.user;
